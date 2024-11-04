7cb59740fa99cb463f688325cdb5cfba26d98bcd
giuse <giuseppedefilippo03@gmail.com>
2024-11-04 18:45:08 +0100

    bonus 5

---
+++ java-script5/java.js
@@ -0,0 +1,12 @@
+/*Stampa le potenze di 2 fino a 1000.*/
+
+console.clear()
+let numero = null;
+for(let i=0; i<=1000; i++){
+    if(2**i <= 1024){
+        console.log(2**i);
+    }
+    
+}
+    
+  
