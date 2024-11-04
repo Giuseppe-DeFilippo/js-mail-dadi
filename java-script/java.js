4d4614aade8a654ca702a471570d342380d59d99
giuse <giuseppedefilippo03@gmail.com>
2024-11-04 14:06:40 +0100

    start

---
+++ java-script/java.js
@@ -0,0 +1,8 @@
+/* Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Non è necessario provvedere alla validazione delle email */
+
+//prompt per chiedere email
+//creare array per le email
